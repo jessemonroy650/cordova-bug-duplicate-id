8dc537bc6d50a39140621b46d27faa6e14dbcf26
Jesse Monroy <jessemonroy650@yahoo.com>
2016-01-21 18:53:45 -0700

    initial entry

---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+    Date: 2015-12-25
+*/
+// This is to have the 'deviceready' code from firing in a webbrowser.
+var device = {platform:'browser'};
+
+var app = {
+    self : {},
+
+    onDeviceReady : function () {
+        //alert("device ready.");
+        if (device.platform === "iOS") {
+            alert("got iOS.");
+            // hide Exit button. They don't have one on iOS devices.
+            // http://www.mzcart.com/javascript-how-to-addremove-css-class-from-a-dom-element/
+            document.getElementById('exitApp').classList.add("hidden");
+            // deals with post-iOS-7 change that covers the status bar
+            // http://coenraets.org/blog/2013/09/phonegap-and-cordova-with-ios-7/
+            document.body.style.marginTop = "20px";
+            // hide the Splash Screen for iOS only
+            navigator.splashscreen.hide();
+        } else if (device.platform == 'Android') {
+            // Get rid of 300ms delay 
+            document.addEventListener('DOMContentLoaded', function() { FastClick.attach(document.body); }, false);
+            //
+            document.getElementById('exitApp').addEventListener('click', function() {
+                app.exit();
+            });
+        } else if (device.platform == 'browser') {
+            document.getElementById('exitApp').addEventListener('click', function() {
+                app.exit();
+            });
+        }
+    },
+    exit : function () {
+        console.log('Called app.exit()');
+        if ('app' in navigator) {
+            navigator.app.exitApp();
+        } else {
+            alert('exit button hit.');
+        }
+    }
+};
+
+//
+// Wait for PhoneGap to load
+document.addEventListener("deviceready", app.onDeviceReady, false);
+
---
+++ buttons.js
@@ -0,0 +1,9 @@
+/*
+    Date: 2016-01-21
+*/
+
+$('#button1').on('click',function() {
+    console.log('#button1'); // alert('#button1');
+    $('#cordova').html('change');
+});
+
